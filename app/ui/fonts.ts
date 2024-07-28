144b93e3096c3cc01eb3cb6cb48048b95746bfa9
BankaixHunterr <bankaixhunter@gmail.com>
2024-07-28 23:29:00 +0530

    first Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,11 @@
+import { Inter, Playwrite_CA, Josefin_Sans, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const josefin_Sans = Josefin_Sans({ subsets: ['latin'] });
+export const playwrite_CA = Playwrite_CA({});
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
+
